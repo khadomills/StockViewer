83f6d9d31c3786cc700f00679ccdaf919265754e
khadomills <MillerNathan@cwu.edu>
2023-11-21 10:52:54 -0800

    single stock update backend feature added
    updated app.py with parameterized route for updating a single stock, and redirecting to it's page. updated StockDataRetriver.py with fetch_one_stock function

--- StockDataRetriever.py
+++ StockDataRetriever.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 
 class StockDataRetriever:
-
     # Static variable for the API key
     api_key = "6dc4b895bdmshcff1c01a07c300ep1ee1b4jsn3733ec473c54"
 
@@ -55,7 +54,7 @@ class StockDataRetriever:
             conn = sqlite3.connect('database.db')
             cursor = conn.cursor()
 
-            # Process the data as needed (e.g., print or store in a database)
+            # Process the data to json format
             data = response.json()
             # print(data)
 
@@ -124,7 +123,7 @@ class StockDataRetriever:
             # Make the API request
             response = requests.get(url, headers=headers, params=querystring)
 
-            # Process the data as needed (e.g., print or store in a database)
+            # Process the data to json format
             data = response.json()
             # print(data)
 
@@ -209,3 +208,165 @@ class StockDataRetriever:
             # Add a delay to comply with API rate limits
             time.sleep(14)
             stock_id += 1
+
+    # Method to fetch overview and historical share data for one stock.
+    def fetch_one_stock(self, stock_id, symbol):
+
+        # API endpoint URL
+        url = "https://alpha-vantage.p.rapidapi.com/query"
+
+        # Headers for the API request
+        headers = {
+            "X-RapidAPI-Key": self.api_key,
+            "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+        }
+
+        # Query parameters for the API request
+        querystring = {"function": "OVERVIEW", "symbol": symbol, "datatype": "json"}
+
+        # Make the API request
+        response = requests.get(url, headers=headers, params=querystring)
+
+        # Process the data to .json format
+        data = response.json()
+        # print(data)
+
+        # Connect to SQLite database
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        # Insert data into the 'stocks' table
+        query = '''
+        INSERT OR REPLACE INTO stock_data (
+            stock_id, symbol, name, desc, CIK, currency, country, sector, industry, address,
+            fiscal_year_end, latest_qtr, market_capitalization, EBITDA, PERatio, PEGRatio,
+            book_value, div_per_share, div_yield, EPS, rev_per_shareTTM, profit_margin,
+            op_marginTTM, return_on_assetsTTM, return_on_equityTTM, revenueTTM, gross_profitTTM,
+            dilutedEPSTTM, qtrly_earnings_growthYOY, qtrly_revenue_growthYOY, analyst_target_price,
+            trailingPE, forwardPE, price_to_sales_ratioTTM, price_to_book_ratio, EV_to_revenue,
+            EV_to_EBITDA, beta, week_high_52, week_low_52, day_moving_average_50,
+            day_moving_average_200, shares_outstanding, div_date, ex_div_date, last_updated
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        '''
+
+        # Get the current date for 'last_updated'
+        current_date = datetime.now().strftime('%Y-%m-%d')
+
+        # Execute the query
+        cursor.execute(query, (
+            stock_id,
+            data['Symbol'],
+            data['Name'],
+            data['Description'],
+            data['CIK'],
+            data['Currency'],
+            data['Country'],
+            data['Sector'],
+            data['Industry'],
+            data['Address'],
+            data['FiscalYearEnd'],
+            data['LatestQuarter'],
+            data['MarketCapitalization'],
+            data['EBITDA'],
+            data['PERatio'],
+            data['PEGRatio'],
+            data['BookValue'],
+            data['DividendPerShare'],
+            data['DividendYield'],
+            data['EPS'],
+            data['RevenuePerShareTTM'],
+            data['ProfitMargin'],
+            data['OperatingMarginTTM'],
+            data['ReturnOnAssetsTTM'],
+            data['ReturnOnEquityTTM'],
+            data['RevenueTTM'],
+            data['GrossProfitTTM'],
+            data['DilutedEPSTTM'],
+            data['QuarterlyEarningsGrowthYOY'],
+            data['QuarterlyRevenueGrowthYOY'],
+            data['AnalystTargetPrice'],
+            data['TrailingPE'],
+            data['ForwardPE'],
+            data['PriceToSalesRatioTTM'],
+            data['PriceToBookRatio'],
+            data['EVToRevenue'],
+            data['EVToEBITDA'],
+            data['Beta'],
+            data['52WeekHigh'],
+            data['52WeekLow'],
+            data['50DayMovingAverage'],
+            data['200DayMovingAverage'],
+            data['SharesOutstanding'],
+            data['DividendDate'],
+            data['ExDividendDate'],
+            current_date,
+        ))
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
+
+        # Print a message indicating that data has been fetched
+        print(f'Fetched Overview Data for {stock_id} ({symbol})')
+
+        # API endpoint URL
+        url = "https://alpha-vantage.p.rapidapi.com/query"
+
+        # Headers for the API request
+        headers = {
+            "X-RapidAPI-Key": self.api_key,
+            "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+        }
+
+        # Query parameters for the API request
+        querystring = {"function": "TIME_SERIES_DAILY", "symbol": symbol, "outputsize": "full", "datatype": "json"}
+
+        # Make the API request
+        response = requests.get(url, headers=headers, params=querystring)
+
+        # Connect to DB
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        # Process the data into json response
+        data = response.json()
+
+        # Iterate over each date in the API response
+        for date, values in data['Time Series (Daily)'].items():
+            # Convert the date to the required format
+            date_formatted = date
+
+            # Check if the date already exists in the database
+            cursor.execute("SELECT COUNT(*) FROM stock_share_prices WHERE stock_id = ? AND time = ?",
+                           (stock_id, date_formatted))
+            count = cursor.fetchone()[0]
+
+            # if count is 0, add share price data from that date into db
+            if count == 0:
+
+                # Insert data into the 'stock_share_prices' table
+                cursor.execute('''                
+                INSERT INTO stock_share_prices (
+                    stock_id, time, open, high, low, close, volume
+                ) VALUES (?, ?, ?, ?, ?, ?, ?);
+                ''',
+                (
+                    stock_id,
+                    date_formatted,
+                    float(values['1. open']),
+                    float(values['2. high']),
+                    float(values['3. low']),
+                    float(values['4. close']),
+                    float(values['5. volume']),
+                ))
+
+            # Data is up-to-date, return
+            else:
+                print(f'Historical share price data now up to date for {stock_id} ({symbol})')
+                break
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
+        # Print a message indicating that data has been fetched
+        print(f'Fetched Historical Share Data for {stock_id} ({symbol})')
